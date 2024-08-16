6d50ae3940f9d79b5aa401345059e5123fe7a2fa
thiago <thiagofreitas1106@gmail.com>
2024-08-16 12:29:14 -0300

    add some styles on category menu component

--- src/components/Categories/styles.ts
+++ src/components/Categories/styles.ts
@@ -1,8 +1,11 @@
 import styled from 'styled-components/native'
+import { Platform } from 'react-native'
 
-export const Category = styled.View`
-align-items: center;
-margin-left: 24px;
+const isAndroid = Platform.OS === 'android'
+
+export const Category = styled.TouchableOpacity`
+  align-items: center;
+  margin-left: 24px;
 `
 
 export const Icon = styled.View`
@@ -13,4 +16,6 @@ export const Icon = styled.View`
   align-items: center;
   justify-content: center;
   margin-bottom: 8px;
+  box-shadow: 0px 2px 1px rgba(0, 0, 0, ${isAndroid ? 1 : 0.1});
+  elevation: 2;
 `
