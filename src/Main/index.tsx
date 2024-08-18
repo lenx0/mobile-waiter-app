2be7e12dfaabbb77d19237ba1fe26c766df4ec5d
thiago <thiagofreitas1106@gmail.com>
2024-08-18 19:56:37 -0300

    OrderHeader

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -21,10 +21,18 @@ export function Main() {
     setSelectedTable(table)
   }
 
+  function handleCancelOrder() {
+    setSelectedTable('')
+
+  }
+
   return (
     <>
       <Container>
-        <Header />
+        <Header
+          selectedTable={selectedTable}
+          onCancelOrder={handleCancelOrder}
+        />
         <CategoriesContainer>
           <Categories />
         </CategoriesContainer>
