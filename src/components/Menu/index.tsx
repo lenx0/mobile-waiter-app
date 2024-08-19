28b743fc7b516ca8e398ff322c446c2497950225
thiago <thiagofreitas1106@gmail.com>
2024-08-18 21:02:10 -0300

    product modal

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,37 +2,55 @@ import { FlatList, TouchableOpacity } from "react-native";
 import { products } from "../../mocks/products";
 import { Text } from "../Text";
 
-import { Product, ProductImage, ProductDetails, Separator, AddToCartButton } from './styles'
+import { ProductContainer, ProductImage, ProductDetails, Separator, AddToCartButton } from './styles'
 import { formatCurrency } from "../utils/formatCurrency";
 import { PlusCircle } from "../Icons/PlusCircle";
+import { ProductModal } from "../ProductModal";
+import { useState } from "react";
+import { Product } from "../../types/Product";
 
 export function Menu() {
 
   console.log(products[0].imagePath)
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  const [selectedProduct, setSelectedProduct] = useState<null | Product>(null)
+
+  function handleOpenModal(product: Product) {
+    setIsModalVisible(true)
+    setSelectedProduct(product)
+  }
+
   return (
-    <FlatList
-      data={products}
-      style={{ marginTop: 32 }}
-      contentContainerStyle={{ paddingHorizontal: 24 }}
-      keyExtractor={product => product._id}
-      ItemSeparatorComponent={Separator}
-      renderItem={({ item: product }) => (
-        <Product>
-          <ProductImage
-            source={{
-              uri: `http://192.168.18.2:3001/uploads/${product.imagePath}`
-            }}
-          />
-          <ProductDetails>
-            <Text weight="600">{product.name}</Text>
-            <Text size={14} color="#666" style={{ marginVertical: 8 }}>{product.description}</Text>
-            <Text weight="600" size={14}>{formatCurrency(product.price)}</Text>
-          </ProductDetails>
-          <AddToCartButton>
-            <PlusCircle />
-          </AddToCartButton>
-        </Product>
-      )}
-    />
+    <>
+      <ProductModal
+        visible={isModalVisible}
+        onClose={() => setIsModalVisible(false)}
+        product={selectedProduct}
+      />
+      <FlatList
+        data={products}
+        style={{ marginTop: 32 }}
+        contentContainerStyle={{ paddingHorizontal: 24 }}
+        keyExtractor={product => product._id}
+        ItemSeparatorComponent={Separator}
+        renderItem={({ item: product }) => (
+          <ProductContainer onPress={() => handleOpenModal(product)}>
+            <ProductImage
+              source={{
+                uri: `http://192.168.18.2:3001/uploads/${product.imagePath}`
+              }}
+            />
+            <ProductDetails>
+              <Text weight="600">{product.name}</Text>
+              <Text size={14} color="#666" style={{ marginVertical: 8 }}>{product.description}</Text>
+              <Text weight="600" size={14}>{formatCurrency(product.price)}</Text>
+            </ProductDetails>
+            <AddToCartButton>
+              <PlusCircle />
+            </AddToCartButton>
+          </ProductContainer>
+        )}
+      />
+    </>
   )
 }
