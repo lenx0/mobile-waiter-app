5b70396f4442a4ce2592f70ba116a0f9247142d5
thiago <thiagofreitas1106@gmail.com>
2024-08-20 09:40:44 -0300

    api integration done

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,5 +1,5 @@
 import { ActivityIndicator } from 'react-native'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Button } from '../components/Button'
 import { Categories } from '../components/Categories'
 import { Header } from '../components/Header'
@@ -17,17 +17,42 @@ import {
 import { Cart } from '../components/Cart'
 import { CartItem } from '../types/CartItem'
 import { Product } from '../types/Product'
-
-import { products as mockProducts } from "../mocks/products";
 import { Empty } from '../components/Icons/Empty'
 import { Text } from '../components/Text'
+import { Category } from '../types/Category'
+import { api } from '../components/utils/api'
 
 export function Main() {
   const [isTableModalVisible, setIsTableModalVisible] = useState(false)
   const [selectedTable, setSelectedTable] = useState('')
   const [cartItems, setCartItems] = useState<CartItem[]>([])
-  const [isLoading] = useState(false)
-  const [products, setProducts] = useState<Product[]>(mockProducts)
+  const [isLoading, setIsLoading] = useState(true)
+  const [products, setProducts] = useState<Product[]>([])
+  const [categories, setCategories] = useState<Category[]>([])
+  const [isLoadingProducts, setIsLoadingProducts] = useState(false)
+
+  useEffect(() => {
+    Promise.all([
+      api.get('/categories'),
+      api.get('/products')
+    ]).then(([categoriesResponse, productsResponse]) => {
+      setCategories(categoriesResponse.data)
+      setProducts(productsResponse.data)
+      setIsLoading(false)
+    })
+  }, [])
+
+  async function handleSelectCategory(categoryId: string) {
+    const route = !categoryId
+      ? '/products'
+      : `/categories/${categoryId}/products`
+
+    setIsLoadingProducts(true)
+
+    const { data } = await api.get(route)
+    setProducts(data)
+    setIsLoadingProducts(false)
+  }
 
   function handleSaveTable(table: string) {
     setSelectedTable(table)
@@ -103,22 +128,34 @@ export function Main() {
         {!isLoading && (
           <>
             <CategoriesContainer>
-              <Categories />
+              <Categories
+                categories={categories}
+                onSelectCategory={handleSelectCategory}
+              />
             </CategoriesContainer>
-            {products.length > 0 ? (
-              <MenuContainer>
-                <Menu
-                  onAddToCart={handleAddToCart}
-                  products={products}
-                />
-              </MenuContainer>
-            ) : (
+
+            {isLoadingProducts ? (
               <CenteredContainer>
-                <Empty />
-                <Text color="#666" style={{ marginTop: 24 }}>
-                  Nenhum produto foi cadastrado
-                </Text>
+                <ActivityIndicator color="#D73035" size="large" />
               </CenteredContainer>
+            ) : (
+              <>
+                {products.length > 0 ? (
+                  <MenuContainer>
+                    <Menu
+                      onAddToCart={handleAddToCart}
+                      products={products}
+                    />
+                  </MenuContainer>
+                ) : (
+                  <CenteredContainer>
+                    <Empty />
+                    <Text color="#666" style={{ marginTop: 24 }}>
+                      Nenhum produto foi cadastrado
+                    </Text>
+                  </CenteredContainer>
+                )}
+              </>
             )}
           </>
         )}
@@ -141,6 +178,7 @@ export function Main() {
               onAdd={handleAddToCart}
               onDecrement={handleDecrementCartItem}
               onConfirmOrder={handleResetOrder}
+              selectedTable={selectedTable}
             />
           )}
         </FooterContainer>
--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -9,16 +9,18 @@ import { MinusCircle } from "../Icons/MinusCircle";
 import { Button } from "../Button";
 import { OrderConfirmedModal } from "../OrderConfirmedModal";
 import { useState } from "react";
+import { api } from "../utils/api";
 
 interface CartProps {
   cartItems: CartItem[]
   onAdd: (product: Product) => void
   onDecrement: (product: Product) => void
   onConfirmOrder: () => void
+  selectedTable: string
 
 }
 
-export function Cart({ cartItems, onAdd, onDecrement, onConfirmOrder }: CartProps) {
+export function Cart({ cartItems, onAdd, onDecrement, onConfirmOrder, selectedTable }: CartProps) {
   const [isLoading, setIsLoading] = useState(false)
   const [isModalVisible, setIsModalVisible] = useState(false)
 
@@ -26,7 +28,18 @@ export function Cart({ cartItems, onAdd, onDecrement, onConfirmOrder }: CartProp
     return acc + cartItem.quantity * cartItem.product.price
   }, 0)
 
-  function handleConfirmOrder() {
+  async function handleConfirmOrder() {
+    setIsLoading(true)
+    const payload = {
+      table: selectedTable,
+      products: cartItems.map((cartItem) => ({
+        product: cartItem.product._id,
+        quantity: cartItem.quantity
+      }))
+    }
+
+    await api.post('/orders', payload)
+    setIsLoading(false)
     setIsModalVisible(true)
   }
 
